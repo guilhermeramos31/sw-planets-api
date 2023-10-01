98cbd00e80260a1a65813e53ccaf615a1b90ca70
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2023-10-01 12:01:49 -0300

    chore(init): project configuration

---
+++ src/main/java/com/compass/pb/swplanetsapi/SwPlanetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.compass.pb.swplanetsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPlanetsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwPlanetsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/compass/pb/swplanetsapi/SwPlanetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.pb.swplanetsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPlanetsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
