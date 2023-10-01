3ae4b9f0a49cae967b2e1e2117aecc89faae6508
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2023-10-01 12:51:40 -0300

    feat(model): created planet entity

---
+++ src/main/java/com/compass/pb/swplanetsapi/models/entity/Planet.java
@@ -0,0 +1,19 @@
+package com.compass.pb.swplanetsapi.models.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "planets")
+public class Planet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String climate;
+    @Column(nullable = false)
+    private String terrain;
+}
