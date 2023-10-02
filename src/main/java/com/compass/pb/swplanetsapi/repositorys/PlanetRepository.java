472155cfdd3dd11b189737980e065cb0dc46ceb6
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2023-10-02 16:06:26 -0300

    refactor(repository): generics added to JpaRepository

--- src/main/java/com/compass/pb/swplanetsapi/repositorys/PlanetRepository.java
+++ src/main/java/com/compass/pb/swplanetsapi/repositorys/PlanetRepository.java
@@ -1,8 +1,9 @@
 package com.compass.pb.swplanetsapi.repositorys;
 
+import com.compass.pb.swplanetsapi.models.entity.Planet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanetRepository extends JpaRepository {
+public interface PlanetRepository extends JpaRepository<Planet, Long> {
 }
