eaa6b6cb0e946e9b5dfce52354e02aed517923b3
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2023-10-02 15:25:25 -0300

    refactor(dtos): removing data annotation from planet request and adding getter and setter annotations

--- src/main/java/com/compass/pb/swplanetsapi/models/dtos/PlanetRequestDTO.java
+++ src/main/java/com/compass/pb/swplanetsapi/models/dtos/PlanetRequestDTO.java
@@ -2,11 +2,15 @@ package com.compass.pb.swplanetsapi.models.dtos;
 
 
 import jakarta.annotation.Nonnull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlanetRequestDTO {
     @Nonnull
     private Long id;
