f1f4a1922aec896a56fd3755448b98f88a071918
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2023-10-02 23:17:47 -0300

    refactor(exception): created constructor for PlanetNotFoundException

--- src/main/java/com/compass/pb/swplanetsapi/exceptions/PlanetNotFoundException.java
+++ src/main/java/com/compass/pb/swplanetsapi/exceptions/PlanetNotFoundException.java
@@ -1,4 +1,16 @@
 package com.compass.pb.swplanetsapi.exceptions;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class PlanetNotFoundException extends RuntimeException{
+    private ErrorCode errorCode;
+    private HttpStatus status;
+
+    public PlanetNotFoundException(){
+        super(ErrorCode.NOT_FOUND.name());
+        this.errorCode = ErrorCode.NOT_FOUND;
+        this.status = HttpStatus.NOT_FOUND;
+    }
 }
